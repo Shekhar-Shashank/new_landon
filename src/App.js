f9dcb29f38a8d8a5ea8f8284d582ca16083f5af0
Shashank <shekhars027@gmail.com>
2021-11-03 10:25:59 +0530

    changed app.js files

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in one day
+          Learn React and use AWS amplify
         </a>
       </header>
     </div>
